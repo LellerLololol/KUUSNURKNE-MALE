388c842f207079692d4b1269541cf20cb077b88d
BHGull <ralfmartin.olev@gmail.com>
2025-02-01 15:03:48 +0200

    piece moving (black don't work)

--- main.py
+++ main.py
@@ -4,41 +4,67 @@ import tkinter
 import draw_board
 import drag_and_drop
 
-def acitivate(seth_magic_man):
+def acitivate(seth_magic_man, white_starts):
     """whimsical board"""
     
     draw_board.draw_board(canvas)
     draw_board.load_pieces(
-        seth_magic_man,
+        seth_magic_man, white_starts,
         white_pawn_image, white_bishop_image, white_knight_image, 
         white_rook_image, white_queen_image, white_king_image, 
         black_pawn_image, black_bishop_image, black_knight_image, 
         black_rook_image, black_queen_image, black_king_image
     )
 
+def bow_buttons():
+    global white_button
+    global black_button
+
+    white_button = tkinter.Button(window, text='Play as white', command=load_white)
+    black_button = tkinter.Button(window, text='Play as black', command=load_black)
+
+    white_button.place(x=50, y=50)
+    black_button.place(x=50, y=100)
+
 def load_pvp():
+    global something
     
     # Load dnd
-    something = drag_and_drop.Example(window, canvas, draw_board.BOARD_LAYOUT, draw_board.BOARD_LENGTH, True)
-    
-    # Load board
-    acitivate(something)
+    something = drag_and_drop.ChessBoardInteraction(window, canvas, draw_board.BOARD_LAYOUT, draw_board.BOARD_LENGTH, True)
 
     # Remove current buttons
     player_button.destroy()
     bot_button.destroy()
+
+    bow_buttons()
 
 def load_bot():
+    global something
     
     # Load dnd
-    something = drag_and_drop.Example(window, canvas, draw_board.BOARD_LAYOUT, draw_board.BOARD_LENGTH, False)
-
-    # Load board
-    acitivate(something)
+    something = drag_and_drop.ChessBoardInteraction(window, canvas, draw_board.BOARD_LAYOUT, draw_board.BOARD_LENGTH, False)
 
     # Remove current buttons
     player_button.destroy()
     bot_button.destroy()
+
+    bow_buttons()
+
+def load_white():
+    
+    # Remove current buttons
+    white_button.destroy()
+    black_button.destroy()
+
+    acitivate(something, True)
+
+def load_black():
+
+    # Remove current buttons
+    white_button.destroy()
+    black_button.destroy()
+
+    acitivate(something, False)
 
 # Tkinter window
 window = tkinter.Tk()
